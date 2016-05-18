7e7eeef5e2e913b38c1d9051c5d68af0c3608435
Dylan Schirino <dylan.schirino@hotmail.fr>
2016-05-18 16:25:48 +0200

    Re-order click model and add register and connexion page

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,7 +21,7 @@ module.exports = function( grunt ) {
                     "src": "css/*.css"
                 },
                 "options": {
-                    "proxy": "localhost:23754", // TODO: adapt to you configuration
+                    "proxy": "localhost:12345", // TODO: adapt to you configuration
                     "watchTask": true
                 }
             }
