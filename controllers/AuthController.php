1befa8941b687a563ec572c5dbb1084c85ae0f80
Dylan Schirino <dylan.schirino@hotmail.fr>
2016-06-01 21:08:19 +0200

    Recovery AuthController

---
+++ controllers/AuthController.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * File: AuthController.php
+ * User: Dylan Schirino
+ * Date: 28/04/16
+ * Time: 16:19
+ */
+namespace Controllers;
+
+use Models\User;
+
+class AuthController{
+
+    private $user_model=null;
+
+    public function __construct(User $user){
+        $this->user_model=$user;
+    }
+    public function getLogin(){
+        return['view'=>'login.php','ressource_title'=>'User login'];
+    }
+    public function getRegister(){
+        return['view'=>'register.php','ressource_title'=>'register new user'];
+    }
+    public function postLogin(){
+        if($user=$this->user_model->check(['username'=>$_POST['username'],'password'=>sha1($_POST['password'])
+        ])){
+            $_SESSION['user']=json_encode($user);
+            header('Location: ?a=admin&r=page');
+            return;
+        }
+        else{
+
+        }
+    }
+    public function postRegister(){
+
+        $errors=[];
+        if (empty($_POST['mail'])) {
+            $errors['mail'] = 'Veuillez entrer un email';
+        }else{
+            if(filter_var($_POST['mail'], FILTER_VALIDATE_EMAIL) == false) {
+                $errors['mail'] = 'Veuillez entrer un email valide';
+            }
+        }
+        if (empty($_POST['password'])) {
+            $errors['password'] = 'Veuillez entrer un mot de passe';
+        }
+
+        if (count($errors)) {
+            $_SESSION['errors'] = $errors;
+            $_SESSION['old_datas'] = $_POST;
+            header('Location: index.php?r=auth&a=getRegister');
+            return; //le header ne sufffit pas il faut mettre en plus un return pour le faire sortir
+        }
+        if ($this->user_model->save([
+            'password' => sha1($_POST['password']),
+            'email' => $_POST['mail'],
+            'username' => $_POST['username']
+        ])
+        ) {
+            return ['view' => 'login.php', 'ressource_title' => 'Please login'];
+        }
+    }
+    public function getLogout(){
+        if(isset($_SESSION['user'])){
+            unset($_SESSION['user']);
+            session_destroy();
+            setcookie(session_name(),'',time()-3600);
+
+        }
+        header('Location: index.php');
+        return;
+    }
+
+}
---
+++ controllers/BookController.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * File: BookController.php
+ * User: Dylan Schirino
+ * Date: 30/05/16
+ * Time: 21:21
+ */
+
+
+namespace Controllers;
+use Models\Authors;
+
+class AuthorController {
+
+    private $authors_model = null;
+
+    // On lie author_model à Author
+    public function __construct()
+    {
+        $this->authors_model = new Authors();
+    }
+    // On crée la fonction index
+    public function index(){
+        $authors = $this->authors_model->all();
+        $page_title='BiblioTECH - Auteurs';
+        $view = 'index_authors.php';
+        return ['author' => $authors, 'page_title' => $page_title, 'view' => $view,];
+    }
+    function show()
+    {
+        if (!isset($_GET['id'])) {
+            die('Il manque l’identifiant de votre Auteurs');
+        }
+        $id = intval($_GET['id']);
+        $authors = $this->authors_model->find($id);
+        $books = null;
+        if (isset($_GET['with'])) {
+            $with = explode(',', $_GET['with']);
+            if (in_array('books', $with)) {
+                $books_model = new Books();
+                $books = $books_model->getBooksByAuthorId($authors->id);
+            }
+        }
+        $editors = null;
+        $page_title = 'la fiche de ' . $authors->name;
+        $view = 'show_authors.php';
+        return [
+            'authors' => $authors,
+            'books' => $books,
+            'editors' => $editors,
+            'page_title' => $page_title,
+            'view' => $view,
+        ];
+    }
+}
